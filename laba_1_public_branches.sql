56f7e2d43973b08eafd38fe083ca87eefac1b1ce
Vasilevykh-M <vasilevyh00@gmail.com>
2021-10-22 13:20:42 +0500

    first commit

---
+++ laba_1_public_branches.sql
@@ -0,0 +1,13 @@
+insert into public.branches (id, branch, count_worker, id_city)
+values  (1, 'Липатова 13', 20, 1),
+        (2, 'Липатова 12', 21, 1),
+        (3, 'Ласьвинская 32', 15, 2),
+        (4, 'Докучаева 42', 2, 3),
+        (5, 'Ленинская слобода 2', 500, 2),
+        (6, 'Broad street 40/44', 21, 4),
+        (7, 'Compton street 42', 21, 4),
+        (8, 'Bombton street ', 21, 5),
+        (9, 'Old Compton Street 42/44', 250, 6),
+        (10, 'Old Compton Street 45/45', 2500, 6),
+        (11, 'Докучаева 1', 5, 7),
+        (12, 'Ленина 40', 5, 7);
---
+++ laba_1_public_country_branch.sql
@@ -0,0 +1,6 @@
+insert into public.country_branch (id, country)
+values  (1, 'Россия'),
+        (2, 'Великобритания'),
+        (3, 'США'),
+        (4, 'Киргизия'),
+        (5, 'ААА');
